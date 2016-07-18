e055f3dea56c607c8ea5de6486792b16d8bbd88b
davisr137 <davisr137@gmail.com>
2016-07-18 13:57:52 -0700

    linear least squares
    add linear least squares example using gradient descent

--- deep-learning/chapter_4.py
+++ deep-learning/chapter_4.py
@@ -41,3 +41,50 @@ def cond_numb(A):
     return(out)
     
 out = cond_numb(A)
+
+# linear least squares example
+# A*x = b
+
+r = 1000
+c = 5
+a = np.random.random([r,c])
+x_true = np.array([0.8,0.9,1,1.1,1.2])
+b = np.zeros(r)
+for i in range(r):
+    s = 0
+    for j in range(c):
+        s += a[i,j]*x_true[j]
+    b[i] = s
+    
+# add noise to predictors
+A = a + np.random.normal(0,0.1,[r,c])
+
+# estimate using numpy 
+out = np.linalg.lstsq(A,b)
+x1 = out[0]
+
+# solve using gradient based optimization
+
+def lsq_grad_desc(A,b,delta,epsilon):
+    [row,col] = A.shape
+    x = np.matrix(np.ones(col))
+    x = x.transpose()
+    grad = A.transpose() * A * x - A.transpose() * b
+    while np.linalg.norm(grad) > delta:
+        grad = A.transpose() * A * x - A.transpose() * b
+        x = x - epsilon*grad
+    return(x)
+    
+delta = 0.0001
+epsilon = 0.001
+
+A = np.matrix(A)
+b = np.matrix(b)
+b = b.transpose()
+
+# observe that we get the same result with gradient descent as with the numpy library routine 
+x2 = lsq_grad_desc(A,b,delta,epsilon)
+
+    
+        
+
